a86d874fcbd4b334fd6e5523f2700d5c1dcdd2bb
sudipkumar0200 <sudipkumar0200@gmail.com>
2025-11-05 19:00:47 +0530

    temp fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  // origin: ['https://todo-app-212w.vercel.app'],
-  origin: ["http://localhost:5173"],
+  origin: ['https://todo-app-212w.vercel.app'],
+  // origin: ["http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
