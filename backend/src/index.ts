07cdec5aa93116e8c88edf0db853c4f474b8d227
thebeast01 <mohammadsaif0847@gmail.com>
2026-02-17 18:11:52 +0530

    Added Cors URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors({
     // origin: ['https://todo-app-212w.vercel.app'],
-    origin: ["https://todo-app-sepia-omega-93.vercel.app","http://localhost:5173"],
+    origin: ["https://todo-app-z7g1.vercel.app","http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
